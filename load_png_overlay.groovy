adf27c2fe5aaffb3437bb77ede0d7e771873dc37
eokoshi <127910259+eokoshi@users.noreply.github.com>
2025-06-20 13:21:23 +0900

    add load_png_overlay script for heatmapping

---
+++ load_png_overlay.groovy
@@ -0,0 +1,42 @@
+/**
+ * Add a 'small' image overlay to the image currently open in the QuPath viewer.
+ * Here, 'small' indicates something that ImageIO can handle, and which isn't pyramidal.
+ *
+ * @author Pete Bankhead
+ * 
+ * Updated 1/2023 to be compatible with QuPath v0.4.x
+ */
+
+
+import qupath.lib.gui.viewer.ImageInterpolation
+import qupath.lib.gui.viewer.overlays.BufferedImageOverlay
+import qupath.lib.regions.ImageRegion
+import qupath.fx.dialogs.FileChoosers
+
+import javax.imageio.ImageIO
+
+import static qupath.lib.gui.scripting.QPEx.*
+
+var file = FileChoosers.promptForFile("PNG Overlay",
+    FileChoosers.createExtensionFilter("PNG Overlay", "*.png"));
+if (file == null) {
+    logger.info("No PNG files selected for import");
+    return;
+}
+
+// Define path to the image file (PNG recommended)
+// def path = 'C:/Users/pathology/Documents/Projects/HPylori/HPylori_qupath/heatmap.png'
+
+def viewer = getCurrentViewer()
+def img = ImageIO.read(file)
+
+// Define the region
+// Here, we assume it covers the entire image - but we can define it some other way (e.g. parsing it from the filename?)
+def region = ImageRegion.createInstance(0, 0, viewer.getServerWidth(), viewer.getServerHeight(), viewer.getZPosition(), viewer.getTPosition())
+
+// Create & add an overlay
+def overlay = new BufferedImageOverlay(viewer.getOverlayOptions(), region, img)
+overlay.setInterpolation(ImageInterpolation.NEAREST)
+// Multiple image regions can be added if necessary
+//overlay.getRegionMap().put(region2, img2)
+viewer.getCustomOverlayLayers().setAll(overlay)
